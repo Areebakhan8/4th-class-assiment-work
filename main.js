85703a46c5319e6b636aca71fadf29b6cb69ae2a
Areebakhan8 <160344226+Areebakhan8@users.noreply.github.com>
2024-03-23 01:04:38 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+import inquirer from "inquirer";
+let answer = await inquirer.prompt([{
+        name: "username",
+        type: "string",
+        message: "what is your name?",
+    }]);
+console.log("welcome!" + answer.username);
+answer = await inquirer.prompt([{
+        name: "age",
+        type: "number",
+        message: " Enter your age?",
+    }]);
+console.log('you are`' + answer.age + '`years old');
